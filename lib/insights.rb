1a6e6d769f9329e9df12361a5fd3c7cbab885522
Naz <nazmalik08@gmail.com>
2017-08-02 20:32:34 +0100

    Compute highest like ratio

--- lib/insights.rb
+++ lib/insights.rb
@@ -5,11 +5,25 @@ class Insights
   def initialize(json_filename)
     file = open(json_filename)
     json = file.read
-    @video_statistics = JSON.parse(json)
+    parsed_json = JSON.parse(json)
+    @video_statistics = parsed_json["videos"]
   end
 
   def compute_likes_ratio(likes, dislikes)
-    (dislikes.to_f / likes.to_f) * 10000
+    total = likes + dislikes
+    (likes.to_f / total.to_f) * 100
+  end
+
+  def highest_like_ratio
+
+    titles_to_ratios = {}
+
+    @video_statistics.each do |video|
+      ratio = compute_likes_ratio(video["likes"], video["dislikes"])
+      titles_to_ratios[video["title"]] = ratio
+    end
+
+    titles_to_ratios.key(titles_to_ratios.values.max)
   end
 
 end
--- spec/insights_spec.rb
+++ spec/insights_spec.rb
@@ -10,15 +10,19 @@ describe Insights do
     it 'parses a JSON file on entry' do
       json = test_file.read
       parsed_json = JSON.parse(json)
-      expect(insights.video_statistics).to eq parsed_json
+      expect(insights.video_statistics).to eq parsed_json["videos"]
     end
   end
 
-  describe '#compute_likes_ratio' do
-    it 'outputs correct percentage' do
-      likes = insights.video_statistics["videos"][0]["likes"]
-      dislikes = insights.video_statistics["videos"][0]["dislikes"]
-      expect(insights.compute_likes_ratio(likes, dislikes)).to be_within(0.01).of(97.6)
+  describe '#compute_likes' do
+    it 'percentage of likes to dislikes for one video' do
+      likes = insights.video_statistics[0]["likes"]
+      dislikes = insights.video_statistics[0]["dislikes"]
+      expect(insights.compute_likes_ratio(likes, dislikes)).to be_within(0.01).of(99.03)
+    end
+
+    it 'returns title of video with the best like ratio' do
+      expect(insights.highest_like_ratio).to eq "Most Likely To : Tanya Edition | Zoella"
     end
   end
 
