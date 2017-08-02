e7987577d8cd4c1b686ef0899475374d83978eea
Naz <nazmalik08@gmail.com>
2017-08-02 22:42:54 +0100

    Move rounding logic out from enlighten_me.rb to insights.rb

--- lib/insights.rb
+++ lib/insights.rb
@@ -14,17 +14,18 @@ class Insights
     title.partition(TITLE_DIVIDER).last
   end
 
-  def video_with_highest_likes_ratio
-    get_titles_to_likes_ratio.key(get_titles_to_likes_ratio.values.max)
+  def get_video_with_highest_likes_ratio
+    get_names_with_likes_ratio.key(get_names_with_likes_ratio.values.max)
   end
 
   def mean_likes_ratio
-    ratios = get_titles_to_likes_ratio.values
-    sum_of_ratios = ratios.inject { |sum, item| sum + item }.to_f
-    sum_of_ratios / ratios.length
+    ratios = get_names_with_likes_ratio.values
+    total = ratios.inject { |sum, item| sum + item }.to_f
+    mean = total / ratios.length
+    mean.round(1)
   end
 
-  def total_views
+  def compute_total_views
     sum = 0
     @video_statistics.each { |video| sum += video["views"] }
     sum
@@ -44,7 +45,7 @@ class Insights
     (likes.to_f / total.to_f) * 100
   end
 
-  def get_titles_to_likes_ratio
+  def get_names_with_likes_ratio
     titles_to_ratios = {}
 
     @video_statistics.each do |video|
